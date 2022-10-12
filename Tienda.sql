29df0376bbec1233966e9011d44c8633794c151f
Luis Florez <braian1k3@gmail.com>
2022-10-12 11:24:17 -0500

    Actualizar archivo de tienda con ingreso de datos y demás opciones de comandos.

--- Tienda.sql
+++ Tienda.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE Tienda;
+﻿CREATE DATABASE Tienda;
 
 use Tienda;
 create table comprador (
@@ -47,17 +47,65 @@ constraint envio_id foreign key (envio_id) references envio(id_envio),
 constraint num_vendedor foreign key (num_vendedor) references vendedor(id_vendedor),
 constraint articulo_id foreign key (articulo_id) references articulo(id_articulo))
 
-use Tienda;
-select * from comprador;
+
+		use Tienda;
+		select * from comprador;
+
+		use Tienda;
+		select * from envio;
+
+		use Tienda;
+		select * from vendedor;
+
+		use Tienda;
+		select * from articulo;
+
+		use Tienda;
+		select * from factura;
+
 
 use Tienda;
-select * from envio;
+alter table vendedor
+add salario money;
 
 use Tienda;
-select * from vendedor;
+alter table envio
+alter column tipo_envio varchar (40);
 
 use Tienda;
-select * from articulo;
+alter table comprador
+drop column tel_comprador;
 
 use Tienda;
-select * from factura;
+exec sp_rename 'facturacion' , 'factura';
+
+use Tienda;​​
+EXEC sp_rename 'dbo.articulo', 'id_articulo', 'articulo';
+
+
+use Tienda;
+delete from comprador where id_comprador=6;
+
+
+use Tienda;
+insert into comprador(nombre_comprador,apellido_comprador,direccion_comprador,tel_comprador) values ('Riguberto','Herrera','Calle 2 # 91',5047142),
+('Jorge','Martínez','Calle 66 # 9',5049461), ('Tatiana','Montoya','Calle 4 # 44',5040010), ('Camila','Palacio','Calle 44 # 4',5041111),
+('Sara','Fernandez','Calle 13 # 1',5041133), ('Paola','Echeverri','Calle 51 # 4',5047890), ('Daniel','Bedoya','Calle 11 # 11',5044050),
+('Andres','Martínez','Calle 54 # 14',5048795), ('Fernando','Montoya','Calle 36 # 40',5041346), ('Reinaldo','Palacio','Calle 27 # 20',5046456),
+('Sofia','Fernandez','Calle 21 # 90',5043918), ('Estefani','Echeverri','Calle 11 # 6',5048271), ('Manuela','Bedoya','Calle 19 # 8',5049898),
+('Sergio','Martínez','Calle 4 # 74',5040502), ('Luis','Montoya','Calle 30 # 1',5043080), ('Rene','Palacio','Calle 81 # 28',5046907),
+('Laura','Fernandez','Calle 22 # 9',5043060), ('Simón','Echeverri','Calle 69 # 7',5043014), ('Raul','Bedoya','Calle 16 # 7',5040287);
+
+use Tienda;
+insert into vendedor(nombre_vendedor,apellido_vendedor,direccion_vendedor,salario) values ('Andrea','Rodríguez','Calle 97 # 79',1000000),
+('Jorge','Moreno','Calle 26 # 26',1000000), ('Tatiana','Torres','Calle 4 # 81',1200000), ('Camila','Medina','Calle 25 # 54',1200000),
+('Hugo','Muñoz','Calle 97 # 11',1400000), ('Mateo','Ortega','Calle 7 # 71',1400000), ('María','Gallego','Calle 45 # 45',1600000),
+('Andres','Moreno','Calle 34 # 43',1600000), ('Fernando','Torres','Calle 21 # 3',1800000), ('Reinaldo','Medina','Calle 36 # 63',1800000),
+('Martín','Muñoz','Calle 9 # 01',1000000), ('Pablo','Ortega','Calle 66 # 6',1000000), ('Isabel','Gallego','Calle 33 # 3',1000000),
+('Sergio','Moreno','Calle 85 # 36',2000000), ('Luis','Torres','Calle 7 # 2',2000000), ('Rene','Medina','Calle 10 # 4',2200000),
+('Lucas','Muñoz','Calle 12 # 21',2200000), ('Paula','Ortega','Calle 41 # 14',2400000), ('Carmen','Gallego','Calle 10 # 5',2400000),
+('Raul','Rodríguez','Calle 29 # 1',2500000);
+
+use Tienda;
+insert into envio(tipo_envio) values ('Envíos urgentes'), ('Envíos estándar'), ('Envíos contra reembolso'), ('Envíos pesados'),
+('Envíos frágiles'), ('Envíos internacionales'), ('Envío a un punto de entrega');
