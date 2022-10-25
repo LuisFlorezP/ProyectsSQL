050bf1a7bab8ecbcbfb8d9a99afd3284260f0966
Luis Florez <braian1k3@gmail.com>
2022-10-25 10:36:14 -0500

    Organizar las consultas de las operaciones de agrupamiento.

--- Tienda/Taller2.sql
+++ Tienda/Taller2.sql
@@ -1,29 +1,29 @@
 use Taller1;
-select sum(precio_articulo) from articulo;
+select sum(precio_articulo) 'SUMA PRECIOS ARTÍCULOS' from articulo;
 
 use Taller1;
-select count(envio_id) from factura where tipo_envio='Envíos urgentes';
+select count(envio_id) 'ENVÍOS URGENTES' from factura where envio_id=1;
 
 use Taller1;
-select sum(neto) from factura where tipo_envio='Envíos pesados';
+select sum(neto) 'ENVÍOS PESADOS' from factura where envio_id=7;
 
 use Taller1;
-select max(precio_articulo) from articulo;
+select max(precio_articulo) 'MAYOR PRECIO' from articulo;
 
 use Taller1;
-select min(precio_articulo) from articulo;
+select min(precio_articulo) 'MENOR PRECIO' from articulo;
 
 use Taller1;
-select avg(neto) from factura;
+select avg(neto) 'PROMEDIO DEL VALOR NETO' from factura;
 
 use Taller1;
-select avg(bruto) from factura;
+select avg(bruto) 'PROMEDIO DEL VALOR BRUTO' from factura;
 
 use Taller1;
-select count(nombre_comprador) from comprador where nombre_comprador='Fernando';
+select count(nombre_comprador) 'CANTIDAD DE FERNANDOS' from comprador where nombre_comprador='Fernando';
 
 use Taller1;
-select count(nombre_vendedor) from vendedor where nombre_vendedor='Andrea';
+select count(nombre_vendedor) 'CANTIDAD DE ANDREAS' from vendedor where nombre_vendedor='Andrea';
 
 use Taller1
 select salario from vendedor where salario between '1000000' and '1400000';
