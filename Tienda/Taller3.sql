ad02aee6c8d3279fc48d346e74903f2689835312
Luis Florez <braian1k3@gmail.com>
2022-10-25 00:10:34 -0500

    Generar la estructura para realizar 20 consultas diferentes con order by, group by y having, además ya se generaron 6 de ellas.

---
+++ Tienda/Taller3.sql
@@ -0,0 +1,59 @@
+use Taller1;
+select * from comprador where nombre_comprador like 'L%' order by apellido_comprador ASC;
+
+use Taller1;
+select * from vendedor where nombre_vendedor like 'M%' order by apellido_vendedor DESC;
+
+use Taller1;
+select * from articulo where nombre_articulo like '%i%' and precio_articulo between '1000000' and '1300000' order by precio_articulo;
+
+use Taller1;
+select * from factura where vendedor_id=1 or vendedor_id=3 or vendedor_id=5 order by neto DESC;
+
+use Taller1;
+select * from factura where bruto>=100000 and bruto<520000 order by neto ASC;
+
+use Taller1;
+select * from articulo where like '%o' and precio_articulo>=500000 order by nombre_articulo DESC;
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
+
+use Taller1;
+select
