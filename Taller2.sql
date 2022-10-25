e2fd319ba5697459cb7d463b8e864b25ba758b12
Luis Florez <braian1k3@gmail.com>
2022-10-24 23:25:48 -0500

    Completar las consultas para esta segunda fase de la base de datos, utilizando las funciones de agrupamiento count(), sum() min(), max() y avg().

---
+++ Taller2.sql
@@ -0,0 +1,26 @@
+use Taller1;
+select sum(precio_articulo) from articulo;
+
+use Taller1;
+select count(envio_id) from factura where tipo_envio='Envíos urgentes';
+
+use Taller1;
+select sum(neto) from factura where tipo_envio='Envíos pesados';
+
+use Taller1;
+select max(precio_articulo) from articulo;
+
+use Taller1;
+select min(precio_articulo) from articulo;
+
+use Taller1;
+select avg(neto) from factura;
+
+use Taller1;
+select avg(bruto) from factura;
+
+use Taller1;
+select count(nombre_comprador) from comprador where nombre_comprador='Fernando';
+
+use Taller1;
+select count(nombre_vendedor) from vendedor where nombre_vendedor='Andrea';
