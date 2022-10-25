39df5eb5d110d7d2f78b6e25196c2cd8f7332208
Luis Florez <braian1k3@gmail.com>
2022-10-24 23:28:48 -0500

    Añadir otro tipo de consulta con betwwen.

--- Taller2.sql
+++ Taller2.sql
@@ -24,3 +24,6 @@ select count(nombre_comprador) from comprador where nombre_comprador='Fernando';
 
 use Taller1;
 select count(nombre_vendedor) from vendedor where nombre_vendedor='Andrea';
+
+use Taller1
+select salario from vendedor where salario between '1000000' and '1400000';
