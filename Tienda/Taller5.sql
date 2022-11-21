c141cc34879f12c133b394520ab90a03338f209e
Luis Florez <braian1k3@gmail.com>
2022-11-21 11:02:18 -0500

    Agregar procedimientos con operaciones y cambios de salario y nombres.

--- Tienda/Taller5.sql
+++ Tienda/Taller5.sql
@@ -8,3 +8,28 @@ select COUNT(id_articulo) 'Cantidad de ventas del artículo',articulo_id,nombre_
 
 -- Mostrar: Ventas de artículo
 execute SP_venArt;
+
+-- Guardar procedimiento: Sumar valor al salario
+create procedure SP_maxSal 
+@id_vendedor as int, @suma as int 
+AS print 'Agregar valor al salario' update vendedor set salario=salario+@suma where id_vendedor=@id_vendedor
+
+-- Guardar procedimiento: Restar valor al salario
+create procedure SP_minSal 
+@id_vendedor as int, @resta as int 
+AS print 'Agregar valor al salario' update vendedor set salario=salario-@resta where id_vendedor=@id_vendedor
+
+-- Guardar procedimiento: Cambiar nombre
+create procedure SP_camNOm
+@id_vendedor as int, @nuevo as varchar(60)
+AS print 'Cambiar nombre' update vendedor set nombre_vendedor=@nuevo where id_vendedor=@id_vendedor
+
+	-- Mostrar: Sumar valor al salario
+	execute SP_maxSal 2,800000
+
+	-- Mostrar: Restar valor al salario
+	execute SP_minSal 2,800000
+
+	-- Mostrar: Cambiar nombre
+	execute SP_camNOm 2,'Tatiana'
+    
