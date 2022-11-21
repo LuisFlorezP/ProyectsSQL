b968d2f13e9e47b313d62bc010f081b33b5bf2b1
Luis Florez <braian1k3@gmail.com>
2022-11-21 10:27:20 -0500

    Finalizar y subir la parte del Taller4 y agregarla a Tienda con el resto de las consultas faltantes.

--- Tienda/Tienda.sql
+++ Tienda/Tienda.sql
@@ -251,3 +251,22 @@ select * from articulo where nombre_articulo like 'A%' order by nombre_articulo;
 -- Ordenar de forma descendente los apellidos de los compradores, mostrando solamente aquellos aepllidos que terminen con la letra "i" de la tabla comprador
 use Tienda;
 select * from comprador where apellido_comprador like '%i' order by apellido_comprador DESC;
+
+
+-- Relación de las tablas vendedor y artículo para ver todo pero de vendedor 20
+select * from vendedor inner join articulo on vendedor.id_vendedor=articulo.vendedor_id where articulo.vendedor_id=20
+
+-- Relación de las tablas vendedor y artículo para ver el agrupamiento de los artículos
+select COUNT(articulo.vendedor_id) 'Contador de artículos',id_articulo from vendedor inner join articulo on vendedor.id_vendedor=articulo.vendedor_id group by id_articulo 
+
+-- Relación de las tablas vendedor y artículo para ver el agrupamiento de los artículos junto al nombre del vendedor
+select COUNT(articulo.vendedor_id) 'Contador de artículos',id_articulo,nombre_vendedor from vendedor inner join articulo on vendedor.id_vendedor=articulo.vendedor_id group by id_articulo,nombre_vendedor 
+
+-- Relación de las tablas vendedor y artículo para ver el agrupamiento de los artículos junto al nombre del vendedor de forma descendente
+select COUNT(articulo.vendedor_id) 'Contador de artículos',id_articulo,nombre_vendedor from vendedor inner join articulo on vendedor.id_vendedor=articulo.vendedor_id group by id_articulo,nombre_vendedor order by id_articulo DESC
+
+-- Relación de las tablas vendedor y artículo para ver el agrupamiento de los vendedores junto a los artículos y vendedores pero de vendedor 20
+select id_articulo 'Num Artículo',id_vendedor 'Num vendedor', COUNT(articulo.vendedor_id) 'Contador de artículos' from vendedor inner join articulo on vendedor.id_vendedor=articulo.vendedor_id group by id_articulo,id_vendedor having id_vendedor=20
+
+-- Relación de las tablas vendedor y factura para ver el agrupamiento de las facturas junto a los vendedores y su dirección pero de vendedor 3
+select COUNT(id_pago) 'Cantidad de facturas',id_vendedor,direccion_vendedor from vendedor inner join factura on vendedor.id_vendedor=factura.num_vendedor group by id_vendedor,direccion_vendedor having id_vendedor=3
